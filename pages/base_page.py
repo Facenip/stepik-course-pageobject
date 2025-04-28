147f9af2599d5d98975d1ddf228a1ec5f205760f
Misha <2004mif@gmail.com>
2025-04-28 12:26:03 +0500

    Первый тест на основе Page Object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,3 @@
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.common.exceptions import NoAlertPresentException
-
-from math import sin, log
-
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from pages.locators import BasePageLocators
-
-
 class BasePage:
 
     def __init__(self, browser, url, timeout = 10):
